1d78ef6716c039d28d3f975bd5c323aa0f10d4ce
jainjatin1234 <jainjatin748@gmail.com>
2024-01-25 22:09:34 +0530

    hhyh

--- controller/PaymentController.js
+++ controller/PaymentController.js
@@ -1,13 +1,13 @@
-require('dotenv').config({ path: './.env' })
-// const key =process.env.STRIPE_SECRET_KEY
-// console.log(key)
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+require('dotenv').config({path: './.env'})
+//const key = process.env.STRIPE_SECRET_KEY
+//console.log(key)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 class PaymentController {
 
-    static processPayment = async (req, res) => {
-        //console.log(req.body)
-        const mypayment = await stripe.paymentIntents.create({
+    static processpayment = async (req,res) =>{
+        // console.log(req.body)
+        const mypayment  = await stripe.paymentIntents.create({
             amount: req.body.amount,
             currency: "inr",
             metadata: {
@@ -15,12 +15,10 @@ class PaymentController {
             }
         })
         console.log(mypayment)
-        res
-            .status(200)
-            .json({ success: true, client_secret: mypayment.client_secret });
-
+        res.status(200).json({Success:true,client_secret: mypayment.client_secret})
     }
-    static sendStripeApiKey = async (req, res) => {
+
+    static sendStripApiKey = async (req,res) =>{
 
         res.status(200).json({
             stripeApiKey: process.env.STRIPE_API_KEY
@@ -28,4 +26,5 @@ class PaymentController {
     }
 
 }
+
 module.exports = PaymentController
